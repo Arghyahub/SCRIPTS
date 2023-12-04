eec7c7d209643bb1f531946b1685b2e750e6fc92
Arghya Das <84245432+Arghyahub@users.noreply.github.com>
2023-12-05 02:35:38 +0530

    Add Tailwind and React router dom

--- react/react.sh
+++ react/react.sh
@@ -1,70 +1,108 @@
 #!/bin/sh
-printf "\nType Y and the script file will be removed automatically\n\n"
+
 npm create vite@latest .
 
-cd public
-rm vite.svg
-
-npm install
-
-cd ../src/
+cd src/
 rm -r assets/
 echo '' > App.css
+cd ..
 
-v=$(cat <<EOF
-html , body ,#root {
-    height: 100%;
-}
-* {
-    margin: 0;
-    padding: 0;
-}
+echo -n "1. Do you need Tailwind? (y/n) "
+read tw
 
-.flrow {
-    display: flex;
-    flex-direction: row;
-}
-.flcol {
-    display: flex;
-    flex-direction: column;
-}
+echo -n "2. Do you need React-Router? (y/n) "
+read rr
 
-.jcen {
-    justify-content: center;
-}
-.acen {
-    align-items: center;
-}
-.tcen {
-    text-align: center;
-}
+# echo -n "1. Do you need  (y/n)"
+# read tw
 
-.border {
-    border: 2px solid red;
-}
-.curpoi {
-    cursor: pointer;
-}
 
-.h100 {
-    height: 100%;
-}
-.w100 {
-    width: 100%;
-}
+# Tailwind related code
 
-EOF
-)
-echo "$v" > index.css
+tsconfig="
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    \"./index.html\",
+    \"./src/**/*.{js,ts,jsx,tsx}\"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+"
 
-v=$(cat <<EOF
-import "./App.css"
+
+tsdir="
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+"
+
+app="
+import \"./App.css\"
 
 const App = () => {
     return (
-        <div className="flrow jcen acen h100">
+        <div className=\"h-screen w-screen bg-red-300 flex flex-row justify-center items-center\">
             <h1>Made with &lt;3 by Arghya Das for you⌀</h1>
         </div>
+    )
+}
+
+export default App;
+"
+
+if [[ $tw=="y" || $tw=='Y' ]]; 
+then
+  npm install -D tailwindcss postcss autoprefixer
+  npx tailwindcss init -p
+  echo "$tsconfig" > tailwind.config.js
+  echo "$tsdir" > src/App.css
+  echo "$app" > src/App.jsx
+fi
+
+
+# React Router related code
+
+app=$(cat <<EOF
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Link
+} from "react-router-dom";
+import "./App.css"
+
+const LinkBtns = () => {
+    return (
+        <div className="flex w-full justify-center gap-4">
+            <Link to={'/'} className="px-2 py-1 bg-green-400 text-white rounded-md" >Home</Link>
+            <Link to={'/demo'} className="px-2 py-1 bg-green-400 text-white rounded-md" >Demo</Link>
+        </div>
+    )
+}
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div className="h-screen w-screen bg-red-300 flex flex-col justify-center items-center gap-10">
+            <h1>Made with &lt;3 by Arghya Das for you⌀</h1>
+            <LinkBtns />
+      </div>
+    },
+    {
+      path: "/demo",
+      element: <div className="h-screen w-screen bg-red-300 flex flex-col justify-center items-center gap-10">
+            <h1>Checkout my other projects <a href="https://cv-arghyahub.vercel.app/">Here</a></h1>
+            <LinkBtns />
+        </div>
+    },
+]);
+
+const App = () => {
+    return (
+        <RouterProvider router={router} />
     )
 }
 
@@ -72,14 +110,14 @@ export default App;
 EOF
 )
 
-if [ -f "App.jsx" ]; then
-    echo "$v" > App.jsx
-else
-    echo "$v" > App.tsx
+if [[ $rr=="y" || $rr=='Y' ]]; 
+then
+  npm install react-router-dom
+  echo "$app" > src/App.jsx
+  echo ""
+  echo "If you are not using not using Tailwind, Ignore the tailwind classes"
+  echo ""
 fi
 
-printf "\nRunning Local Server...\n\n"
 
 npm run dev
-
-echo -e "Made with \xE2\x9D\xA4 by Arghya Das for you\xE2\x8C\x80"
